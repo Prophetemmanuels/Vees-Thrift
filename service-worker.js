4f60897bf3d512d9ae85895a208e6b53ec62c38f
Prophetemmanuels <ghopbusinesscenter@gmail.com>
2026-01-31 21:25:28 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,23 @@
-const CACHE_NAME = 'vees-thrift-cache-v1';
+const CACHE_NAME = "vees-thrift-cache-v1";
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './images/logo.png',
-  './images/p1.jpg',
-  './images/p2.jpg',
-  './images/p3.jpg'
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./service-worker.js"
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache=>{
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(response=>{
+      return response || fetch(e.request);
+    })
   );
 });
