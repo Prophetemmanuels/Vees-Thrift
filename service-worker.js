f3c44349e7a0a7c3f5a5400fead885f878117bf5
Prophetemmanuels <ghopbusinesscenter@gmail.com>
2026-01-31 17:21:16 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE="vees-thrift-v1";
+const ASSETS=[
+ "index.html",
+ "manifest.json",
+ "images/icon.png",
+ "images/icon-192.png",
+ "images/icon-512.png"
+];
+
+self.addEventListener("install",e=>{
+ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+ self.skipWaiting();
+});
+
+self.addEventListener("fetch",e=>{
+ e.respondWith(
+  caches.match(e.request).then(r=>r||fetch(e.request))
+ );
+});
