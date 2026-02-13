5388e7723b09065821da529d71e570d3c2e1bec4
Prophetemmanuels <ghopbusinesscenter@gmail.com>
2026-02-13 17:29:07 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,17 @@
-const CACHE_NAME = "vees-thrift-v1";
-
+const CACHE_NAME = 'vees-thrift-cache-v1';
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json"
+  './',
+  './index.html',
+  './manifest.json',
+  './images/logo.png',
+  './images/icon-192.png',
+  './images/icon-512.png'
 ];
 
-// Install Service Worker
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-// Activate
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Fetch
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
